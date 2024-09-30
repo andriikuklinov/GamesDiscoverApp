a8bb7146987bac9e06fa4644ef1967c31b083f5e
akuklinov <akuklinov@outlook.com>
2024-09-30 15:57:10 +0300

    Optimize images.

--- games-discover-app/src/components/GameCard/GameCard.tsx
+++ games-discover-app/src/components/GameCard/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react"
 import { Game } from "../../hooks/useGames"
 import PlatformIconList from "../PlatformIconList/PlatformIconList"
 import CriticScore from "../CriticScore/CriticScore"
+import getCroppedImageUrl from "../../services/common"
 
 interface Props{
     game: Game
@@ -9,7 +10,7 @@ interface Props{
 export default function GameCard({ game }: Props){
     return <>
         <Card borderRadius={10} overflow='hidden'>
-            <Image src={game.background_image} />
+            <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
                 <HStack justifyContent='space-between'>
