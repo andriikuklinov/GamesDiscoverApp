ddbef499e42a51b7353e501aea5f005d9cfe64b1
akuklinov <akuklinov@outlook.com>
2024-09-30 12:10:17 +0300

    Implement navbar.

--- games-discover-app/src/App.tsx
+++ games-discover-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,9 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"` //1024px
       }}>
-        <GridItem area="nav" bg="coral">Nav</GridItem>
+        <GridItem area="nav">
+          <Navbar />
+        </GridItem>
         <Show above="lg">
           <GridItem area="aside" bg="gold">Aside</GridItem>
         </Show>
