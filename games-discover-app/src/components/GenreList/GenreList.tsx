624959ec14645137f91f5f9cc6c7796c07a67c97
akuklinov <akuklinov@outlook.com>
2024-09-30 17:14:47 +0300

    Display genres.

--- games-discover-app/src/components/GenreList/GenreList.tsx
+++ games-discover-app/src/components/GenreList/GenreList.tsx
@@ -1,10 +1,18 @@
-import useData from "../../hooks/useData";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "../../hooks/useGenres";
+import getCroppedImageUrl from "../../services/common";
 
 
 export default function GenreList(){
     const {data} = useGenres();
     return <>
-        {data.map(genre=> <li key={genre.id}>{genre.name}</li>)}
+        <List>
+            {data.map(genre=> <ListItem key={genre.id} paddingY='5px'>
+                <HStack>
+                    <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
+                    <Text fontSize='lg'>{genre.name}</Text>
+                </HStack>
+            </ListItem>)}
+        </List>
     </>
 }
