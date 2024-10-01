00b399b0651ef82640c459e8ef01de9f09e49723
akuklinov <akuklinov@outlook.com>
2024-10-01 14:40:44 +0300

    Fix game cards spacing.

--- games-discover-app/src/components/GameCard/GameCard.tsx
+++ games-discover-app/src/components/GameCard/GameCard.tsx
@@ -12,11 +12,11 @@ export default function GameCard({ game }: Props){
         <Card>
             <Image height='219px' src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
-                <Heading fontSize='2xl'>{game.name}</Heading>
-                <HStack justifyContent='space-between'>
+                <HStack justifyContent='space-between' marginBottom={3}>
                     <PlatformIconList platforms={game.parent_platforms.map((obj)=> obj.platform)}/>
                     <CriticScore score={game.metacritic} />
                 </HStack>
+                <Heading fontSize='2xl'>{game.name}</Heading>
             </CardBody>
         </Card>
     </>
