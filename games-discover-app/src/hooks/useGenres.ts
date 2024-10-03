cda209dca574556c63f09c165c095e91cf3ed4c4
akuklinov <akuklinov@outlook.com>
2024-10-03 15:27:18 +0300

    Refactor: create lookup hooks.

--- games-discover-app/src/hooks/useGenres.ts
+++ games-discover-app/src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
-//import genresStatic from "../services/genres-static";
+import genresStatic from "../services/genres-static";
 
 const apiClient = new APIClient<Genre>('/genres');
 
@@ -14,8 +14,8 @@ export default function useGenres(){
     //return useData<Genre>('/genres');
     return useQuery({
         queryKey: ['genres'],
-        queryFn: ()=> apiClient.getAll({}),
+        queryFn: () => apiClient.getAll({}),
         staleTime: 24 * 60 * 60 * 1000, //24h
-        //initialData: { count: genresStatic.length, results: genresStatic}
+        initialData: genresStatic
     })
 }
--- games-discover-app/src/hooks/usePlatforms.ts
+++ games-discover-app/src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import platformsStatic from "../services/platforms-static";
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
@@ -14,5 +15,6 @@ export default function usePlatforms(){
         queryKey: ['platforms'],
         queryFn: ()=> apiClient.getAll({}),
         staleTime: 24 * 60 * 60 * 1000, //24h
+        initialData: platformsStatic
     })
 }
