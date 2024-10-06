fa1478ae71c325ff4e8998ad1d9e07ca9cc07753
akuklinov <akuklinov@outlook.com>
2024-10-06 15:07:12 +0300

    Implement redirection while clicking logo.

--- games-discover-app/src/components/Navbar/Navbar.tsx
+++ games-discover-app/src/components/Navbar/Navbar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from '../../assets/react.svg';
 import ColorSwitch from "../ColorSwitch/ColorSwitch";
 import SearchInput from "../SearchInput/SearchInput";
+import { Link } from "react-router-dom";
 
 export default function Navbar(){
     return <>
         <HStack justifyContent='space-between' padding='10px'>
-            <Image src={logo} boxSize="40px"/>
+            <Link to='/'>
+                <Image src={logo} boxSize="40px" objectFit='cover'/>
+            </Link>
             <SearchInput/>
             <ColorSwitch />
         </HStack>
